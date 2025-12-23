8fbb7508b501519133099190cf3d1414f9d6745d
clasak <cody.lytle@prestox.com>
2025-12-23 09:07:36 -0600

    Fix Vercel build: skip husky in CI and build frontend workspace

--- src/frontend/src/modules/sales/components/index.ts
+++ src/frontend/src/modules/sales/components/index.ts
@@ -10,9 +10,20 @@ export { LogActivityDialog } from './LogActivityDialog';
 
 // Lead components
 export { LeadConversionDialog } from './LeadConversionDialog';
+export { default as LeadForm } from './LeadForm';
 
 // Opportunity components
 export { NextStepAlert, NextStepIndicator } from './NextStepAlert';
+export { default as DealForm } from './DealForm';
+
+// Contact components
+export { default as ContactForm } from './ContactForm';
+
+// Quote components
+export { default as QuoteBuilder } from './QuoteBuilder';
+export { default as QuotePreview } from './QuotePreview';
+export { default as QuoteSendDialog } from './QuoteSendDialog';
+export { default as QuoteVersionHistory } from './QuoteVersionHistory';
 
 // Re-export any existing components
 // Add more exports as components are created
